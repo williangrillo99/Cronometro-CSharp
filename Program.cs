5052f1129349cc25fa0cb824a467d3dbda0e7e5c
willian-grillo <willian.grillo@autoglass.com.br>
2022-07-12 09:17:48 -0300

    Criação do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+Projeto Cronometro realizado no curso do balta 
